7a97e5304fbe0ac381c1f59593030456cf8035d6
Dinhsiduyet <dinhsyduyet@outlook.com>
2020-11-27 19:56:35 +0700

    new commit

--- main.js
+++ main.js
@@ -23,3 +23,23 @@ function prev(){
     slide.innerHTML = "<img src =" + image[i - 1] + ".jpg>";
 }
 setInterval(next , 2000);
+
+
+
+function onReady(callback) {
+    var intervalId = window.setInterval(function() {
+      if (document.getElementsByTagName('body')[0] !== undefined) {
+        window.clearInterval(intervalId);
+        callback.call(this);
+      }
+    }, 4000);
+  }
+  
+  function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+  }
+  
+  onReady(function() {
+    setVisible('.wrap-slide', true);
+    setVisible('.container', false);
+  });
